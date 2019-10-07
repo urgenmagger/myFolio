dc5c429aa8a54d97249af9f1d33e2b17a6eff66d
urgenmagger <evgen.magger@gmail.com>
2019-10-07 17:21:18 +0300

    add grid and flex

--- src/components/MyTemplate.js
+++ src/components/MyTemplate.js
@@ -2,8 +2,15 @@ import React from 'react';
 import '../App.css'; //..level up
 import {Link} from 'react-router-dom';
 import Button from '@material-ui/core/Button';
+//import pageContent from '../content/pageContent.js';
 
 function MyTemplate(props) {
+  //const tech = pageContent.technologies.map(items => (
+  //<ul>
+  //<li>{items}</li>
+  //console.log(items);
+  //</ul>
+  //));
   return (
     <div>
       <div className="container">
@@ -11,13 +18,19 @@ function MyTemplate(props) {
           <h2>{props.content.rubric}</h2>
           <img className="imgsvg" src={props.content.image} alt="" />
           <div class="page_oriview">
-            <h3>Обзор</h3>
-            <p>{props.content.content}></p>
-            <Link to={props.content.path}>
-              <Button variant="contained" color="primary">
-                <li>{props.content.linkButton}</li>
-              </Button>
-            </Link>
+            <div className="left_side">
+              <h3>Обзор</h3>
+              <p>{props.content.content}></p>
+              <Link to={props.content.path}>
+                <Button variant="contained" color="primary" size="large">
+                  {props.content.linkButton}
+                </Button>
+              </Link>
+              <p> technologies: {props.content.technologies}</p>
+            </div>
+            <div className="note">
+              <p>{props.content.note}</p>
+            </div>
           </div>
         </div>
       </div>
--- src/content/pageContent.js
+++ src/content/pageContent.js
@@ -1,31 +1,34 @@
-
 const pageContent = [
-    {
-      id: 1,
-      rubric: 'ПРОСТОЙ ОПТИМИЗАТОР ДЛЯ SVG PATH',
-      image: require("../img/example.svg"),
-      content:'Данная оптимизация целесообразна в случае избыточного кода после точки в координатах path. В основном такой код получается при работе с редактором Inkscape (в Inkscape нужно также нажать кнопочку "упростить" или Ctrl+L, без этого бывают непредсказуемые результаты), в Adobe Illustrator после точки наблюдал два знака, оптимизировать можно на 3-8%.',
-  
-      preview:'link from github and button',
-      path:'/svgopt',
-      linkButton:'opt',
-    },
-    {
-      id: 3,
-      rubric: '3',
-      image: 'url',
-      content:'here will be unique content',
-      preview:'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
-      linkButton:'url',
-    },
-    {
-      id: 4,
-      rubric: '5',
-      image: 'url',
-      content:'llllllllllllllllllllllllllllll',
-      preview:'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
-      linkButton:'url',
-    }
-  ];
+  {
+    id: 1,
+    rubric: 'ПРОСТОЙ ОПТИМИЗАТОР ДЛЯ SVG PATH',
+    image: require('../img/example.svg'),
+    content:
+      'Данная оптимизация целесообразна в случае избыточного кода после точки в координатах path. В основном такой код получается при работе с редактором Inkscape (в Inkscape нужно также нажать кнопочку "упростить" или Ctrl+L, без этого бывают непредсказуемые результаты), в Adobe Illustrator после точки наблюдал два знака, оптимизировать можно на 3-8%.',
+
+    preview: 'link from github and button',
+    path: '/svgopt',
+    linkButton: 'DEMO',
+    note: 'pet project изначально был сделан для собстевнных нужд',
+    //technologies: [{tech:"jQuery"}, {tech:"regular expressions"}],
+    technologies: 'jQuery, regular expressions',
+  },
+  {
+    id: 3,
+    rubric: '3',
+    image: require('../img/example.svg'),
+    content: 'here will be unique content',
+    preview: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+    linkButton: 'url',
+  },
+  {
+    id: 4,
+    rubric: '5',
+    image: 'url',
+    content: 'llllllllllllllllllllllllllllll',
+    preview: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+    linkButton: 'url',
+  },
+];
 
 export default pageContent;
