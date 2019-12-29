1d0c69ee69b426ece054ee439974c0875df210e8
urgenmagger <evgen.magger@gmail.com>
2019-12-29 22:13:09 +0300

    add to header grid

--- src/pageComponents/nav.js
+++ src/pageComponents/nav.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import '../App.css';
-import logo from '../img/logo.svg';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import TwitterIcon from '@material-ui/icons/Twitter';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import TelegramIcon from '@material-ui/icons/Telegram';
 import Logo from '../img/logo.jsx';
 import {Link} from 'react-router-dom';
 
@@ -9,17 +12,49 @@ function nav() {
     <nav>
       <div className="container">
         <div className="container-grid">
-          <Link to="/">
-            <Logo color="#a87952" size = {80}/>
-          </Link>
-          <ul className="nav-links">
-            <Link to="/about">
-              <li>About</li>
+          <div className="nav_img">
+            <Link to="/">
+              <Logo color="#a87952" />
             </Link>
-            <Link to="/folio">
-              <li>Folio</li>
-            </Link>
-          </ul>
+          </div>
+          <div className="nav_info">
+            <ul className="nav-links">
+              <Link to="/about">
+                <li>About</li>
+              </Link>
+              <Link to="/folio">
+                <li>Folio</li>
+              </Link>
+            </ul>
+            <h1>Привет ворлд...</h1>
+            <p>
+              Я Евгений, друзья зовут меня Urgen. Это мое первое SPA написаное с
+              нуля на React JS. Вобщем lorem ipsum друзья.
+            </p>
+            <p>Эй, сей веселей, вправо сей, влево сей… (АБС)</p>
+            <ul className="nav_social">
+              <li>
+                <a href="https://www.linkedin.com/in/evgen-megger-5aa783125/">
+                  <LinkedInIcon />
+                </a>
+              </li>
+              <li>
+                <a href="https://twitter.com/MEGGER_I">
+                  <TwitterIcon />
+                </a>
+              </li>
+              <li>
+                <a href="https://github.com/urgenmagger/">
+                  <GitHubIcon />
+                </a>
+              </li>
+              <li>
+                <a href="https://t.me/Magger_Urgen">
+                  <TelegramIcon />
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </nav>
